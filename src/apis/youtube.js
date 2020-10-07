45d590cc7387ab19eaf217d80771db43f6a83cde
GuzlejM <martin.guzlej@gmail.com>
2020-10-07 12:39:22 +0100

    VideoList component added

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyB9SS85VwQbh9j7u-fUpWe3K8-3m0d6GX0';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+});
