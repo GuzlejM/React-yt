b9971c2cc7a2afe633987ed3f55e638ca0998ecc
GuzlejM <martin.guzlej@gmail.com>
2020-10-07 15:25:20 +0100

    Video Preview

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
 
 class App extends React.Component {
     state = { 
@@ -19,14 +21,26 @@ class App extends React.Component {
     };
 
     onVideoSelect = video => {
-        console.log('From the App!', video)
+        this.setState({ selectedVideo: video });
     }
 
     render() {
         return (
             <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">        
+                            <VideoList 
+                                onVideoSelect={this.onVideoSelect} 
+                                videos={this.state.videos}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         ) 
     }
